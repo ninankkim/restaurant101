5f16fd56020a1c3cb520b4ec7c733957ea04cd81
ninankkim <69530297+ninankkim@users.noreply.github.com>
2020-10-07 21:37:18 -0400

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    restaurants (name, distance, stars, categories, favorite_dish, takeout, ate_last)
+
+VALUES
+    ('Dereks Taco Stand', 5000, 4, 'Mexican Fusion', 'Asada Tacos', FALSE, '2019-08-02 12:30:00'),
+    ('Andrew Gas Station', 20, 5, 'Cheap Foods', 'Cheetos', FALSE, '2020-07-14 13:45:00'),
+    ('Thor Thai Palace', 30, 4, 'Thai', 'Noods', TRUE, '2020-07-09 18:00:00'),
+    ('Jackson Cheeseburgers', 25, 4, 'Burgers', 'Sloppy Joes', TRUE, '2016-09-02 03:00:00'),
+    ('Bargavi Sushi Joint', 4500, 5, 'Sushi', 'Dragon Roll', FALSE, '2006-05-03 4:00:00');
